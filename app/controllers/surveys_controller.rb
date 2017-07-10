06cbd364e900059f73b8b0114e89332241efc925
mikebrown24 <mikebrownn@hotmail.com>
2017-07-10 23:59:28 +0000

    commit

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,14 @@
+class SurveysController < ApplicationController
+  def index
+    render 'index'
+  end
+
+  def result
+     session[:count] ||= 0
+      @name = params[:name]
+      @location =params[:location]
+      @language = params[:language]
+      @comment = params[:comment]
+      flash[:success] = "Thanks for submitting this form! You have submitted this form #{session[:count]+=1} times now."
+  end
+end
